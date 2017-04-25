d43871610cded72190284c39063a600d1a23c9d0
Froilan Macugay <froilan.macugay@pictureworks.com.au>
2017-04-25 13:58:21 +1000

    enable CORS in spring boot
    Reference: https://spring.io/guides/gs/rest-service-cors/

---
+++ src/main/java/com/macwebapps/authentication/config/GlobalCorsConfig.java
@@ -0,0 +1,25 @@
+package com.macwebapps.authentication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by froilan.macugay@pictureworks.com.au on 25/4/17.
+ */
+@Configuration
+public class GlobalCorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200", "http://localhost:63342");
+                //registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+}
